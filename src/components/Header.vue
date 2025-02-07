379d2e00eb890a2769fd600091efddff411d32d0
Daniel Jun <dhj190001@utdallas.edu>
2025-02-06 20:03:08 -0500

    Header Component + Navbar

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import DarkModeToggle from './DarkModeToggle.vue';
+import NavBar from './NavBar.vue';
+</script>
+
+<template>
+  <div class="flex items-center justify-between w-full dark:text-white py-10">
+    <div>
+      <a href="#top">
+        <h1 class="font-bold">Daniel Jun.</h1>
+      </a>
+    </div>
+    <div class="flex items-center justify-start gap-10">
+      <NavBar />
+    </div>
+  </div>
+</template>
