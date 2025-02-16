711db74d07e5dd0c168e7368ed297663b5eb9fc1
danjun01 <danjun01@users.noreply.github.com>
2025-02-15 22:39:30 -0500

    Changed anchor tags to RouterLink for vue router usage

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import NavBar from './NavBar.vue';
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
   <div class="flex items-center justify-between w-full dark:text-white py-10">
     <div>
-      <a href="#intro">
-        <h1 class="font-bold">Daniel Jun.</h1>
-      </a>
+      <RouterLink to="/#intro" replace>
+        <h1 class="font-bold">Daniel Jun</h1>
+      </RouterLink>
     </div>
     <div class="flex items-center justify-start gap-10">
       <NavBar />
