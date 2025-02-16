c82a580986388165cb9b129c353bad8291ed48a3
danjun01 <danjun01@users.noreply.github.com>
2025-02-15 22:40:21 -0500

    styling changees

--- src/components/ui/card/Card.vue
+++ src/components/ui/card/Card.vue
@@ -11,7 +11,7 @@ const props = defineProps<{
   <div
     :class="
       cn(
-        'rounded-xl border border-zinc-200 bg-white text-zinc-950 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-50',
+        'rounded-xl border border-zinc-200 bg-white text-zinc-950 dark:border-zinc-800 dark:bg-zinc-900 dark:text-zinc-50',
         props.class,
       )
     "
