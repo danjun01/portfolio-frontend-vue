183077b567d7e59d516633825575f2eb68d5d123
danjun01 <danjun01@users.noreply.github.com>
2025-02-15 22:39:55 -0500

    swapped from createMemHistory to createWebHistory so URL updates with redirects/rerenders

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,29 @@
 import { createApp } from 'vue'
 import './assets/index.css'
 import App from './App.vue'
-import { createMemoryHistory, createRouter } from 'vue-router';
+import { createWebHistory, createRouter } from 'vue-router';
 import HomeView from './components/HomeView.vue';
+import BlogView from './components/BlogView.vue';
 
 const routes = [
   { path: '/', component: HomeView },
+  { path: '/blog/', component: BlogView },
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
-  routes,
-})
+  history: createWebHistory(),
+  routes,  
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      console.log("Scrolling to ", to.hash, " from " , from.hash);
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+      };
+    }
+    return { top: 0 };
+  },
+});
 
 const app = createApp(App)
 app.use(router)
